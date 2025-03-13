35656961a22885744ad819d0ceda9714c72332a2
Tomiris <s330029@studenti.polito.it>
2025-03-13 17:18:21 +0100

    fix: adjust output to match expected format in output.json

--- RouteAnalyzer/src/main/kotlin/com/carrental/Main.kt
+++ RouteAnalyzer/src/main/kotlin/com/carrental/Main.kt
@@ -5,8 +5,9 @@ import com.carrental.loadConfig
 
 import com.carrental.computeMaxDistanceFromStart
 import com.carrental.findMostFrequentedArea
-import com.carrental.countWaypointsOutsideGeofence
 import com.carrental.computeMostFrequentedAreaRadius
+import com.carrental.findWaypointsOutsideGeofence
+import com.carrental.computeOutsideGeofenceCenter
 
 
 fun main() {
@@ -14,26 +15,28 @@ fun main() {
     val config = loadConfig("custom-parameters.yml")
 
     // Compute the maximum distance from the starting point using the Haversine formula
-    val maxDistanceFromStart = computeMaxDistanceFromStart(waypoints, config.earthRadiusKm)
-    // Determine the most frequented area based on the provided waypoints
-    val mostFrequentedArea = findMostFrequentedArea(waypoints)
-    // Count the number of waypoints that are outside the defined geofence area
-    val waypointsOutsideGeofence =
-        countWaypointsOutsideGeofence(
-            waypoints,
-            config.earthRadiusKm,
-            config.geofenceCenterLatitude,
-            config.geofenceCenterLongitude,
-            config.geofenceRadiusKm
-        )
+    val (farthestWaypoint, maxDistanceFromStart) = computeMaxDistanceFromStart(waypoints, config.earthRadiusKm)
+    // Determine the most frequented area and entries count based on the provided waypoints
+    val (mostFrequentedArea, mostFrequentedEntriesCount) = findMostFrequentedArea(waypoints)
     // Compute the radius of the most frequented area, using a default if not provided in the configuration
     val mostFrequentedAreaRadiusKm = computeMostFrequentedAreaRadius(
         config.mostFrequentedAreaRadiusKm,
         maxDistanceFromStart
     )
+    // Compute waypoints that are outside the defined geofence area
+    val waypointsOutside = findWaypointsOutsideGeofence(
+        waypoints,
+        config.earthRadiusKm,
+        config.geofenceCenterLatitude,
+        config.geofenceCenterLongitude,
+        config.geofenceRadiusKm)
+    val waypointsOutsideCenterPoint = computeOutsideGeofenceCenter(waypointsOutside)
+    val waypointsOutsideCount = waypointsOutside.size
+
+
     println("Waypoints: $waypoints")
-    println("Max Distance from Start: $maxDistanceFromStart km")
-    println("Most Frequented Area: $mostFrequentedArea")
-    println("Outside a specified geo-fence: $waypointsOutsideGeofence")
-    println("Most Frequented Area Radius: $mostFrequentedAreaRadiusKm")
+    println("Max Distance from Start: $maxDistanceFromStart km. Farthest Waypoint: $farthestWaypoint")
+    println("Most Frequented Area: $mostFrequentedArea. Most Frequented Entries Count: $mostFrequentedEntriesCount. Most Frequented Area Radius: $mostFrequentedAreaRadiusKm")
+    println("Points outside a specified geo-fence: $waypointsOutside. Center of Points: $waypointsOutsideCenterPoint. Number of points: $waypointsOutsideCount")
+
 }
