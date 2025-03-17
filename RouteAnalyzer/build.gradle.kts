7d65bd5ccda88d667ea1b89beadd1f85b64f3cbe
Yann Villellas <yann.villellas@studenti.polito.it>
2025-03-17 21:59:12 +0100

    feat!: add Dockerfile for containerized application

--- RouteAnalyzer/build.gradle.kts
+++ RouteAnalyzer/build.gradle.kts
@@ -26,3 +26,11 @@ kotlin {
 application {
     mainClass = "com.carrental.MainKt"
 }
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "com.carrental.MainKt"
+    }
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
+}
