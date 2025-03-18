ce2e6f6ce7bf6de9c3ae6c786999e5259d97a960
Setareh Neshatdoust <162510801+Setareh-Neshatdoust@users.noreply.github.com>
2025-03-19 02:21:23 +0330

    fix (resolve comments): delete extra files and libraries

--- RouteAnalyzer/src/main/kotlin/com/carrental/Main.kt
+++ RouteAnalyzer/src/main/kotlin/com/carrental/Main.kt
@@ -1,16 +1,5 @@
 package com.carrental
 
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import java.io.File
-import com.carrental.parseCsv
-import com.carrental.loadConfig
-import com.carrental.computeMaxDistanceFromStart
-import com.carrental.findMostFrequentedArea
-import com.carrental.computeMostFrequentedAreaRadius
-import com.carrental.findWaypointsOutsideGeofence
-import com.carrental.computeOutsideGeofenceCenter
-
 fun main() {
     val waypoints = parseCsv("waypoints.csv")
     val config = loadConfig("custom-parameters.yml")
